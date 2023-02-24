c75222f6864e4c9e09868769e1a175ed325590fc
Dimitar Todorov <d.todorov.vn@gmail.com>
2023-02-24 22:12:50 +0200

    Add thymeleaf and bootstrap support. Make login page to the app.

--- src/main/java/com/dst/restaurantmanagement/repositories/ConsumableRepository.java
+++ src/main/java/com/dst/restaurantmanagement/repositories/ConsumableRepository.java
@@ -1,8 +1,9 @@
 package com.dst.restaurantmanagement.repositories;
 
+import com.dst.restaurantmanagement.models.entities.Consumable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConsumableRepository extends JpaRepository<ConsumableRepository, Long> {
+public interface ConsumableRepository extends JpaRepository<Consumable, Long> {
 }
