d83f6713b8b34d59f80deb1a167c35c04d882d29
Dimitar Todorov <d.todorov.vn@gmail.com>
2023-03-11 16:50:03 +0200

    Add wrong credentials message in login page

--- src/main/java/com/dst/restaurantmanagement/controllers/AuthController.java
+++ src/main/java/com/dst/restaurantmanagement/controllers/AuthController.java
@@ -1,9 +1,12 @@
 package com.dst.restaurantmanagement.controllers;
 
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/auth")
@@ -16,5 +19,13 @@ public class AuthController {
     @PostMapping("/login")
     public String login() {
         return "redirect:/";
+    }
+
+    @PostMapping("/login-error")
+    public String onFailedLogin(@ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY) String username, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, username);
+        redirectAttributes.addFlashAttribute("bad_credentials", true);
+
+        return "redirect:/auth/login";
     }
 }
