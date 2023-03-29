d64a18c4942ed5e26578da7eb3196c4b7b3f031e
Dimitar Todorov <d.todorov.vn@gmail.com>
2023-03-29 11:11:43 +0300

    Add methods for start and finish cooking in cook controller

---
+++ src/main/java/com/dst/restaurantmanagement/controllers/CookingController.java
@@ -0,0 +1,29 @@
+package com.dst.restaurantmanagement.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/cook")
+public class CookingController {
+    @GetMapping
+    public String getCookingPage() {
+        return "cook-page";
+    }
+
+    @GetMapping("/start")
+    public String startCooking(@RequestParam Long id){
+        System.out.println(id);
+
+        return "redirect:/cook";
+    }
+
+    @GetMapping("/finish")
+    public String finishCooking(@RequestParam Long id){
+        System.out.println(id);
+
+        return "redirect:/cook";
+    }
+}
